3273a123b42c984e85895cc86bac6c6a67ef7abb
Hatem <zehani.hatem@gmail.com>
2019-04-25 04:18:50 +0200

    Updates

---
+++ precache-manifest.0d1377c8211d33517d66c33a202a5f93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "965a779b867e7df0b979",
+    "url": "/static/css/main.736bac5d.chunk.css"
+  },
+  {
+    "revision": "965a779b867e7df0b979",
+    "url": "/static/js/main.7fe17caf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b61d40cc5bb05038db5",
+    "url": "/static/js/2.ef5921ba.chunk.js"
+  },
+  {
+    "revision": "60e7572896118351eea144e7e86f5c09",
+    "url": "/static/media/wind.60e75728.svg"
+  },
+  {
+    "revision": "7123d39a80ac22a263e5f398471946a7",
+    "url": "/static/media/humidity.7123d39a.svg"
+  },
+  {
+    "revision": "8cf1b6adb989aa42a82f4a84dab3c029",
+    "url": "/static/media/geo.8cf1b6ad.svg"
+  },
+  {
+    "revision": "5e80e34d0cdfab96d17d4cf7f963c047",
+    "url": "/index.html"
+  }
+];
