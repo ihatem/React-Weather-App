d43824ca3463d097507c3d86b50ffb235fbf6cbc
ihatem <zehani.hatem@gmail.com>
2019-11-05 23:17:49 +0100

    replaced fetch with axios (safari support)

--- src/App.js
+++ src/App.js
@@ -1,92 +1,96 @@
-import React, { Component } from 'react';
-import Head from './components/head'
-import Weather from './components/weather'
+import React, { Component } from "react";
+import Head from "./components/head";
+import Weather from "./components/weather";
+
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends Component {
-
   state = {
     position: {
       lat: undefined,
       lon: undefined,
-      name: undefined,
-    }, 
+      name: undefined
+    },
     temp: {
-      fahr: undefined, 
+      fahr: undefined,
       cel: undefined,
       humidity: undefined,
       wind: undefined,
       summary: undefined,
       subSummary: undefined,
-      icon: 'CLEAR_DAY',
-    }, 
-  }
+      icon: "CLEAR_DAY"
+    }
+  };
 
-  getWeather = async (geo) => {
+  getWeather = async geo => {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = `${proxy}https://api.darksky.net/forecast/${API_KEY}/${this.state.position.lat},${this.state.position.lon}`;
 
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const apiCall = await fetch(`${proxy}https://api.darksky.net/forecast/${API_KEY}/${this.state.position.lat},${this.state.position.lon}`);
-    const data = await apiCall.json();
-
-    this.setState((prevState) => ({
-      position: {
-        ...prevState.position,
-        name: geo ? data.timezone : this.state.position.name,
-      },
-      temp: {
-        fahr: Math.round(data.currently.temperature),
-        cel: Math.round((data.currently.temperature - 32) * 5/9),
-        humidity: data.currently.humidity,
-        wind: data.currently.windSpeed,
-        summary: data.currently.summary, 
-        subSummary: data.hourly.summary,
-        icon: data.currently.icon.replace(/-/g,"_").toUpperCase(),
-      }
-    }));
-  }
+    axios
+      .get(url)
+      .then(res => {
+        const { data } = res;
+        this.setState(prevState => ({
+          position: {
+            ...prevState.position,
+            name: geo ? data.timezone : this.state.position.name
+          },
+          temp: {
+            fahr: Math.round(data.currently.temperature),
+            cel: Math.round(((data.currently.temperature - 32) * 5) / 9),
+            humidity: data.currently.humidity,
+            wind: data.currently.windSpeed,
+            summary: data.currently.summary,
+            subSummary: data.hourly.summary,
+            icon: data.currently.icon.replace(/-/g, "_").toUpperCase()
+          }
+        }));
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   getGeoPos = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-          pos => {
-            this.setState((prevState) => ({
-              position: {
-                ...prevState.position,
-                lat: pos.coords.latitude, 
-                lon: pos.coords.longitude,
-              },
-              temp: {
-                ...prevState.temp,
-              }
-            }));
-            this.getWeather(true); 
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.setState(prevState => ({
+          position: {
+            ...prevState.position,
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
+          },
+          temp: {
+            ...prevState.temp
           }
-      );
-    } else { 
-        alert("Geolocation is not supported by this browser.");
+        }));
+        this.getWeather(true);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
-  }
+  };
 
   componentDidMount() {
     this.getGeoPos();
   }
 
-
   render() {
     return (
       <div className="App">
-        <Head 
+        <Head
           position={this.state.position}
           temp={this.state.temp}
           getWeather={this.getWeather}
           getGeoPos={this.getGeoPos}
         />
-        <Weather 
+        <Weather
           position={this.state.position}
           temp={this.state.temp}
           getWeather={this.getWeather}
-        /> 
+        />
       </div>
     );
   }
